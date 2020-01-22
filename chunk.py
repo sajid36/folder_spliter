fd4372024c9b6c4c34dc9d785307eb5af2547d12
sajid36 <sajidulislam52@yahoo.com>
2020-01-22 02:59:58 -0500

    working

---
+++ chunk.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+import math
+
+source = "/home/sajid/Desktop/zeus/"
+chunk=150
+files = os.listdir(source)
+total = len(files)
+part = math.ceil((total/chunk))
+for subdir in range(0, part):
+	moved = 0
+	command = 'mkdir '+source+"part"+str(subdir)
+	os.system(command)
+	files = [x for x in os.listdir(source) if os.path.isfile(source+x)]
+	for f in files:
+		if moved < chunk:
+			dest = source+"part"+str(subdir)
+			command = 'mv '+source+f+ ' '+dest
+			os.system(command)
+			moved = moved + 1
+		else:
+			continue
+
+
